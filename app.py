2ee89c866046d65b7cc8a3a22e8ba3b391e94bc8
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-04-15 20:31:24 -0600

    fix: :bug: production enviroment

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from api.ocr import ocr_api
 from api.enhance import enhance_api
 from api.auto_correct_perspective import auto_perspective_api
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -10,5 +14,7 @@ app.register_blueprint(auto_perspective_api, url_prefix='/api')
 app.register_blueprint(ocr_api, url_prefix='/api')
 app.register_blueprint(enhance_api, url_prefix='/api')
 
+PRODUCTION = os.getenv('PRODUCTION', False)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=not PRODUCTION, host='0.0.0.0', port=5000)

2d8f1e431d11bb683649cc566d9eeb13a54592dc
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-04-15 20:46:51 -0600

    fix: :bug: production default true

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.register_blueprint(auto_perspective_api, url_prefix='/api')
 app.register_blueprint(ocr_api, url_prefix='/api')
 app.register_blueprint(enhance_api, url_prefix='/api')
 
-PRODUCTION = os.getenv('PRODUCTION', False)
+PRODUCTION = os.getenv('PRODUCTION', True)
 
 if __name__ == '__main__':
     app.run(debug=not PRODUCTION, host='0.0.0.0', port=5000)
